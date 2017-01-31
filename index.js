44809a37117f316cf9c9973a35197bf9fd783e83
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2017-01-31 09:13:01 +0800

    Changed pubsub service name from captureScreenshot to createScreenshot

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ var logger = require('./lib/logger');
 // Here's where the Controller methods actually get hooked to signals from
 // the outside world via Google PubSub api.
 
+var pubsub_options = {};
 if (process.env.NODE_ENV == 'local-test') {
-    var pubsub_options = {
+    pubsub_options = {
         projectId: 'mimetic-codex-781',
         test: true
-    }
+    };
 } else {
     pubsub_options = { projectId: 'mimetic-codex-781' };
 }
@@ -24,16 +25,16 @@ var path = require('path');
 var appRoot = path.resolve(__dirname);
 var crawler = require('./lib/crawler.js');
 
-screenshotPubSub.subscriptions.captureScreenshot(function(err, subscription) {
-    if (err) throw new Error('Error creating subscription to captureScreenshot topic: ' + err);
+screenshotPubSub.subscriptions.createScreenshot(function(err, subscription) {
+    if (err) throw new Error('Error creating subscription to createScreenshot topic: ' + err);
     // message listener
     subscription.on('message', function(message) {
         var websiteURL = message.data;
         crawler.captureScreen(websiteURL, appRoot);
-        logger.info(`Received captureScreenshot message for website: ${websiteURL}`);
+        logger.info(`Received createScreenshot message for website: ${websiteURL}`);
     });
     subscription.on('error', function(err) {
-        logger.error('Error subscribing to captureScreenshot topic, will not be able to receive signals until this is fixed');
+        logger.error('Error subscribing to createScreenshot topic, will not be able to receive signals until this is fixed');
         logger.error(err);
     });
 });
