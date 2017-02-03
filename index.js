154f858eed5dba2142071115f638fef0021ccbe1
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2017-02-03 12:17:41 +0800

    1. Removed message parsing function in crawler.js 2. handled the message attributes as a json object

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ screenshotPubSub.subscriptions.createScreenshot(function(err, subscription) {
     if (err) throw new Error('Error creating subscription to createScreenshot topic: ' + err);
     // message listener
     subscription.on('message', function(message) {
-        var messageContent = message.data;
-        crawler.captureScreen(messageContent, appRoot);
-        logger.info(`Received createScreenshot message with info: ${messageContent}`);
+        var captureScreenInfo = message.attributes;
+        crawler.captureScreen(captureScreenInfo, appRoot);
+        logger.info(`Received createScreenshot message to scrape website: ${captureScreenInfo.websiteURL}`);
     });
     subscription.on('error', function(err) {
         logger.error('Error subscribing to createScreenshot topic, will not be able to receive signals until this is fixed');
