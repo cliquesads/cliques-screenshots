d7498c53124764127bc46041af7e4fc5eabc7ce5
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2017-11-08 10:35:26 +0800

    Make logger information more verbose and consistent for debugging

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var handleMessageLater = function() {
     setTimeout(function() {
         if (dataStore.numberOfRunningPhantomInstances < dataStore.MAX_NUMBER_PHANTOM_INSTANCES) {
             var earliestMessageInPool = dataStore.messagePool.shift();
-    	    logger.info('Number of phantom instances: ' + dataStore.numberOfRunningPhantomInstances);
+            logger.info(`Number of phantom instances is ${dataStore.numberOfRunningPhantomInstances} when dealing with the following screenshot request ------ websiteUrl: ${earliestMessageInPool.attributes.websiteUrl}, pid: ${earliestMessageInPool.attributes.pid}, crgId: ${earliestMessageInPool.attributes.crgId}`);
             dataStore.numberOfRunningPhantomInstances ++;
             return crawler.captureScreen(earliestMessageInPool.attributes, appRoot)
             .then(function() {
@@ -52,25 +52,33 @@ var handleMessageLater = function() {
 };
 
 screenshotPubSub.subscriptions[topic](function(err, subscription) {
-    if (err) throw new Error('Error creating subscription to ' + topic + ' topic: ' + err);
+    if (err) {
+        logger.error(`Error creating subscription to ${topic} topic: ${err}`);
+        throw new Error(`Error creating subscription to ${topic} topic: ${err}`);
+    }
     // message listener
     subscription.on('message', function(message) {
         var captureScreenInfo = message.attributes;
-        logger.info(`Received ${topic} message to scrape website: ${captureScreenInfo.websiteUrl}, pid: ${captureScreenInfo.pid}, crgId: ${captureScreenInfo.crgId}`);
+        logger.info(`Received ${topic} message to capture screenshot: ------ websiteUrl: ${captureScreenInfo.websiteUrl}, pid: ${captureScreenInfo.pid}, crgId: ${captureScreenInfo.crgId}`);
         if (dataStore.numberOfRunningPhantomInstances < dataStore.MAX_NUMBER_PHANTOM_INSTANCES) {
-    	    logger.info('Number of phantom instances: ' + dataStore.numberOfRunningPhantomInstances);
+    	    logger.info(`Number of phantom instances is ${dataStore.numberOfRunningPhantomInstances} when dealing with the following screenshot request: ------ websiteUrl: ${captureScreenInfo.websiteUrl}, pid: ${captureScreenInfo.pid}, crgId: ${captureScreenInfo.crgId}`);
             dataStore.numberOfRunningPhantomInstances ++;
             return crawler.captureScreen(captureScreenInfo, appRoot)
             .then(function() {
                 dataStore.numberOfRunningPhantomInstances --;
             });
         } else {
-            logger.info('Reached maximum allowed phantom instances, save message and handle it later');
+            logger.info(`Reached maximum allowed phantom instances, save the following message and handle it later: ------ websiteUrl: ${captureScreenInfo.websiteUrl}, pid: ${captureScreenInfo.pid}, crgId: ${captureScreenInfo.crgId}`);
             dataStore.messagePool.push(message);
         }
     });
     subscription.on('error', function(err) {
-        logger.error('Error subscribing to ' + topic + ' topic, will not be able to receive signals until this is fixed');
-        logger.error(err);
+        var errorString;
+        if (typeof err === 'string') {
+            errorString = err;
+        } else {
+            errorString = JSON.stringify(err);
+        }
+        logger.error(`Error subscribing to ${topic} topic, will not be able to receive signals until this is fixed. Error message: ${errorString}`);
     });
 });
