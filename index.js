81f1af820328654aec4d0073bad467c2578ebe0a
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2017-11-10 08:46:41 +0800

    1. Number of phantom instances are now strictly controled by system PIDs 2. If there're too many messages received, those messages that cannot be handled immediately will be saved into mongodb for later use. 3. when a crawler finished capturing screenshot, a `FINISH` event will be emitted so the system checks if there're any vacancies for another phantom instance and if there're any unhandled messages in mongodb 4. After an unhandled message fetched from mongodb, it will be removed from mongodb immediately

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var config = require('config');
 var appRoot = path.resolve(__dirname);
 var crawler = require('./lib/crawler.js');
 var dataStore = require('./lib/dataStore.js');
+var promise = require('bluebird');
+var db = require('./lib/connections').EXCHANGE_CONNECTION;
+var models = require('@cliques/cliques-node-utils').mongodb.models,
+    screenshotModels = new models.ScreenshotModels(db);
 
 /* ---------------- SCREENSHOT PUBSUB INSTANCE & LISTENERS ----------------- */
 
@@ -28,28 +32,41 @@ if (process.env.NODE_ENV == 'local-test') {
 }
 var screenshotPubSub = new ScreenshotPubSub(pubsub_options);
 
-var handleMessageLater = function() {
-    if (dataStore.messagePool.length === 0) {
-        // No unhandled message exists
-        return;
+var EventEmitter = require('events');
+var emitter = new EventEmitter();
+// `FINISH` event will be emitted when a phantom instance exists
+emitter.on('FINISH', function() {
+    if (dataStore.numberOfPhantoms < dataStore.MAX_NUMBER_PHANTOM_INSTANCES) {
+        var fetchedMessage = null;
+        var screenshotModels = new models.ScreenshotModels(db);
+        screenshotModels.ScreenshotMessage.promisifiedFindOne = promise.promisify(screenshotModels.ScreenshotMessage.findOne);
+        return screenshotModels.ScreenshotMessage.promisifiedFindOne()
+        .then(function(message) {
+            if (message) {
+                fetchedMessage = message;
+                // message fetched, remove from database immediately
+                screenshotModels.ScreenshotMessage.promisifiedRemove = promise.promisify(screenshotModels.ScreenshotMessage.remove);
+                return screenshotModels.ScreenshotMessage.promisifiedRemove(message)
+                .then(function() {
+                    logger.info(`Message fetched and removed: ------ ${JSON.stringify(fetchedMessage)}`);
+                });
+            } else {
+                return;
+            }
+        })
+        .then(function() {
+            if (fetchedMessage) {
+                dataStore.numberOfPhantoms ++;
+                return crawler.captureScreen(fetchedMessage, appRoot, db)
+                .then(function() {
+                    dataStore.numberOfPhantoms --;
+                    logger.info(`Finished crawling for the following message: ${JSON.stringify(fetchedMessage)}`);
+                    emitter.emit('FINISH');
+                });
+            }
+        });
     }
-    // There are at least 1 unhandled message, check 1 sec later to see if
-    // it is allowed to create phantomjs instance to capture screenshot,
-    // otherwise wait for another 1 sec before check again
-    setTimeout(function() {
-        if (dataStore.numberOfRunningPhantomInstances < dataStore.MAX_NUMBER_PHANTOM_INSTANCES) {
-            var earliestMessageInPool = dataStore.messagePool.shift();
-            logger.info(`Number of phantom instances is ${dataStore.numberOfRunningPhantomInstances} when dealing with the following screenshot request ------ websiteUrl: ${earliestMessageInPool.attributes.websiteUrl}, pid: ${earliestMessageInPool.attributes.pid}, crgId: ${earliestMessageInPool.attributes.crgId}`);
-            dataStore.numberOfRunningPhantomInstances ++;
-            return crawler.captureScreen(earliestMessageInPool.attributes, appRoot)
-            .then(function() {
-                dataStore.numberOfRunningPhantomInstances --;
-            });
-        } else {
-            handleMessageLater();
-        }
-    }, 1000);
-};
+});
 
 screenshotPubSub.subscriptions[topic](function(err, subscription) {
     if (err) {
@@ -58,18 +75,21 @@ screenshotPubSub.subscriptions[topic](function(err, subscription) {
     }
     // message listener
     subscription.on('message', function(message) {
-        var captureScreenInfo = message.attributes;
-        logger.info(`Received ${topic} message to capture screenshot: ------ websiteUrl: ${captureScreenInfo.websiteUrl}, pid: ${captureScreenInfo.pid}, crgId: ${captureScreenInfo.crgId}`);
-        if (dataStore.numberOfRunningPhantomInstances < dataStore.MAX_NUMBER_PHANTOM_INSTANCES) {
-    	    logger.info(`Number of phantom instances is ${dataStore.numberOfRunningPhantomInstances} when dealing with the following screenshot request: ------ websiteUrl: ${captureScreenInfo.websiteUrl}, pid: ${captureScreenInfo.pid}, crgId: ${captureScreenInfo.crgId}`);
-            dataStore.numberOfRunningPhantomInstances ++;
-            return crawler.captureScreen(captureScreenInfo, appRoot)
+        var websiteInfo = message.attributes;
+        logger.info(`Received ${topic} message to capture screenshot: ------ websiteUrl: ${websiteInfo.websiteUrl}, pid: ${websiteInfo.pid}, crgId: ${websiteInfo.crgId}`);
+        if (dataStore.numberOfPhantoms < dataStore.MAX_NUMBER_PHANTOM_INSTANCES) {
+            dataStore.numberOfPhantoms ++;
+            return crawler.captureScreen(websiteInfo, appRoot, db)
             .then(function() {
-                dataStore.numberOfRunningPhantomInstances --;
+                dataStore.numberOfPhantoms --;
+                logger.info(JSON.stringify(websiteInfo) + ' FINISHED crawling');
+                emitter.emit('FINISH'); 
             });
         } else {
-            logger.info(`Reached maximum allowed phantom instances, save the following message and handle it later: ------ websiteUrl: ${captureScreenInfo.websiteUrl}, pid: ${captureScreenInfo.pid}, crgId: ${captureScreenInfo.crgId}`);
-            dataStore.messagePool.push(message);
+            logger.info(`Reached maximum allowed phantom instances, save the following message and handle it later: ------ websiteUrl: ${websiteInfo.websiteUrl}, pid: ${websiteInfo.pid}, crgId: ${websiteInfo.crgId}`);
+            var screenshotModels = new models.ScreenshotModels(db);
+            var newMessage = new screenshotModels.ScreenshotMessage(websiteInfo);
+            newMessage.save();
         }
     });
     subscription.on('error', function(err) {
