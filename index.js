bb1935a4793e9972d1714b8f5ba7e52203a60107
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2018-08-07 16:44:07 +0800

    Make sure only one chromium instance is running, multiple page in this chromium browser can coexist though

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 /* jshint node: true */
 'use strict';
 
-var node_utils = require('@cliques/cliques-node-utils'),
-    ScreenshotPubSub = node_utils.google.pubsub.ScreenshotPubSub;
-var logger = require('./lib/logger');
-var path = require('path');
-var config = require('config');
-var appRoot = path.resolve(__dirname);
-var crawler = require('./lib/crawler.js');
-var promise = require('bluebird');
-var db = require('./lib/connections').EXCHANGE_CONNECTION;
+const node_utils = require('@cliques/cliques-node-utils'),
+    ScreenshotPubSub = node_utils.google.pubsub.ScreenshotPubSub,
+    logger = require('./lib/logger'),
+    path = require('path'),
+    config = require('config'),
+    appRoot = path.resolve(__dirname),
+    crawler = require('./lib/crawler.js'),
+    promise = require('bluebird'),
+    db = require('./lib/connections').EXCHANGE_CONNECTION;
 
-var redis = require('redis'),
+const redis = require('redis'),
     client = redis.createClient();
 promise.promisifyAll(redis.RedisClient.prototype);
+
+const puppeteer = require('puppeteer');
+
 
 /**
  * The allowed maximum number of chromium instances running concurrently 
@@ -29,8 +32,8 @@ var numberOfChromiums = 0;
 
 // Here's where the Controller methods actually get hooked to signals from
 // the outside world via Google PubSub api.
-var projectId = config.get('Screenshots.google.projectId');
-var topic = config.get('Screenshots.google.topic');
+const projectId = config.get('Screenshots.google.projectId'),
+    topic = config.get('Screenshots.google.topic');
 
 var pubsub_options = {};
 if (process.env.NODE_ENV == 'local-test') {
@@ -59,63 +62,69 @@ function parseScreenshotMessageFromRedis(messageString) {
     return message;
 }
 
-var EventEmitter = require('events');
-var emitter = new EventEmitter();
-// `FINISH` event will be emitted when a chromium instance exists
-emitter.on('FINISH', function() {
-    if (numberOfChromiums < MAX_NUMBER_CHROMIUM_INSTANCES) {
-        // Retrieve a screenshot message from redis, 
-        // after rpop command, the message will be removed from 
-        // redis `${topic}` list automatically
-        return client.rpopAsync(`${topic}`)
-        .then(function(messageString) {
-            if (messageString) {
-                var fetchedMessage = parseScreenshotMessageFromRedis(messageString);
-                logger.info(`Message fetched and removed: ------ ${messageString}`);
+return puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+})
+.then(chromiumBrowser => {
+    const EventEmitter = require('events');
+    var emitter = new EventEmitter();
+    // `FINISH` event will be emitted when a chromium instance exists
+    emitter.on('FINISH', function() {
+        if (numberOfChromiums < MAX_NUMBER_CHROMIUM_INSTANCES) {
+            // Retrieve a screenshot message from redis, 
+            // after rpop command, the message will be removed from 
+            // redis `${topic}` list automatically
+            return client.rpopAsync(`${topic}`)
+            .then(function(messageString) {
+                if (messageString) {
+                    var fetchedMessage = parseScreenshotMessageFromRedis(messageString);
+                    logger.info(`Message fetched and removed: ------ ${messageString}`);
+                    numberOfChromiums ++;
+                    return crawler.captureScreen(fetchedMessage, appRoot, db, chromiumBrowser)
+                    .then(function() {
+                        numberOfChromiums --;
+                        logger.info(`Finished crawling for the following message: ${messageString}`);
+                        emitter.emit('FINISH');
+                    });
+                }
+            });
+        }
+    });
+
+    screenshotPubSub.subscriptions[topic](function(err, subscription) {
+        if (err) {
+            logger.error(`Error creating subscription to ${topic} topic: ${err}`);
+            throw new Error(`Error creating subscription to ${topic} topic: ${err}`);
+        }
+        // message listener
+        subscription.on('message', function(message) {
+            var websiteInfo = message.attributes;
+            message.ack();
+            var messageString = `websiteUrl:${websiteInfo.websiteUrl},pid:${websiteInfo.pid},crgId:${websiteInfo.crgId}`;
+            logger.info(`Received ${topic} message to capture screenshot: ------ ${messageString}`);
+            if (numberOfChromiums < MAX_NUMBER_CHROMIUM_INSTANCES) {
                 numberOfChromiums ++;
-                return crawler.captureScreen(fetchedMessage, appRoot, db)
+                return crawler.captureScreen(websiteInfo, appRoot, db, chromiumBrowser)
                 .then(function() {
                     numberOfChromiums --;
-                    logger.info(`Finished crawling for the following message: ${messageString}`);
-                    emitter.emit('FINISH');
+                    logger.info(`${messageString} FINISHED crawling`);
+                    emitter.emit('FINISH'); 
                 });
+            } else {
+                logger.info(`Reached maximum allowed chromium instances, save the following message and handle it later: ------ ${messageString}`);
+                // Save screenshot message to redis in a LIST named `${topic}`
+                return client.lpushAsync(`${topic}`, `${messageString}`);
             }
         });
-    }
+        subscription.on('error', function(err) {
+            var errorString;
+            if (typeof err === 'string') {
+                errorString = err;
+            } else {
+                errorString = JSON.stringify(err);
+            }
+            logger.error(`Error subscribing to ${topic} topic, will not be able to receive signals until this is fixed. Error message: ${errorString}`);
+        });
+    });
 });
 
-screenshotPubSub.subscriptions[topic](function(err, subscription) {
-    if (err) {
-        logger.error(`Error creating subscription to ${topic} topic: ${err}`);
-        throw new Error(`Error creating subscription to ${topic} topic: ${err}`);
-    }
-    // message listener
-    subscription.on('message', function(message) {
-        var websiteInfo = message.attributes;
-        message.ack();
-        var messageString = `websiteUrl:${websiteInfo.websiteUrl},pid:${websiteInfo.pid},crgId:${websiteInfo.crgId}`;
-        logger.info(`Received ${topic} message to capture screenshot: ------ ${messageString}`);
-        if (numberOfChromiums < MAX_NUMBER_CHROMIUM_INSTANCES) {
-            numberOfChromiums ++;
-            return crawler.captureScreen(websiteInfo, appRoot, db)
-            .then(function() {
-                numberOfChromiums --;
-                logger.info(`${messageString} FINISHED crawling`);
-                emitter.emit('FINISH'); 
-            });
-        } else {
-            logger.info(`Reached maximum allowed chromium instances, save the following message and handle it later: ------ ${messageString}`);
-            // Save screenshot message to redis in a LIST named `${topic}`
-            return client.lpushAsync(`${topic}`, `${messageString}`);
-        }
-    });
-    subscription.on('error', function(err) {
-        var errorString;
-        if (typeof err === 'string') {
-            errorString = err;
-        } else {
-            errorString = JSON.stringify(err);
-        }
-        logger.error(`Error subscribing to ${topic} topic, will not be able to receive signals until this is fixed. Error message: ${errorString}`);
-    });
-});
